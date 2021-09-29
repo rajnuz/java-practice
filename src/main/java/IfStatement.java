921b83ae77bc7da5f39e7a1ab376b82ba2d550d4
rajnuz <yu.yf@hotmail.com>
2021-09-29 23:00:26 +0800

    add some javba basics

---
+++ src/main/java/IfStatement.java
@@ -0,0 +1,20 @@
+/**
+ * @author rajnuz
+ * @date 2021/9/27 13:25
+ */
+public class IfStatement {
+    public static void main(String[] args) {
+        int age = 30;
+
+        if (age>0 && age <12){
+            System.out.println("child");
+        }
+        else if (age >12 && age <18){
+            System.out.println("youth");
+        }
+        else{
+            System.out.println("mature");
+        };
+
+    }
+}
