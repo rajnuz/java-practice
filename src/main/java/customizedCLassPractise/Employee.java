2f06d51b880c9d6f11e477cb08b2586a1b1570ce
rajnuz <yu.yf@hotmail.com>
2021-11-21 23:04:12 +0800

    add some code

--- src/main/java/customizedCLassPractise/Employee.java
+++ src/main/java/customizedCLassPractise/Employee.java
@@ -7,9 +7,10 @@ import java.time.LocalDate;
  */
 public class Employee {
     //instance fields
-    private String name; //private 关键字可以确保只有本类的方法可以访问这些实例域，一般不要把实例域标记为public，避免其他类改写从而破坏封装性
+    private final String name; //private 关键字可以确保只有本类的方法可以访问这些实例域，一般不要把实例域标记为public，避免其他类改写从而破坏封装性
     private  double salary;
     private LocalDate hireDay;
+    private static int nextId = 1;// static域是所有同一类对象共享的，不属于某一个类实体。即使类没有实例对象，这个域也存在
 
     //constructor
     //构造器总是通过new方法来实现的
@@ -24,12 +25,19 @@ public class Employee {
         return name;
     }
 
+//    public String setName(String n){return this.name = n;}//final域不可以有set方法
+
     public double getSalary(){
         return salary;
     }
 
     public LocalDate getHireDay(){
         return hireDay;
+    }
+
+
+    public int getNextId (){
+        return nextId;
     }
 
     public void raiseSalary(double byPercent){
---
+++ src/main/java/customizedCLassPractise/EmployeeTest.java
@@ -0,0 +1,26 @@
+package customizedCLassPractise;
+
+/**
+ * @author rajnuz
+ * @date 2021/11/21 22:17
+ */
+public class EmployeeTest {
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[2];
+        staff[0] = new Employee("Carl",75000,1987,12,15);
+        staff[1] = new Employee("tom",75000,1982,1,15);
+
+        for (Employee e : staff){
+            e.raiseSalary(5);
+        }
+
+
+        for (Employee e:staff
+             ) {
+            System.out.println("name=" + e.getName()+ ", salary = " + e.getSalary() + ",hireDay = " + e.getHireDay());
+        }
+
+        System.out.println("staff0 's nextID = " + staff[0].getNextId());
+        System.out.println("staff1 's nextID = " + staff[1].getNextId());
+    }
+}
