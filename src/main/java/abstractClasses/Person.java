28999ab35a79d66f3d6da4b97996856dca3c2c92
Yu <yifan.yu@siemens.com>
2021-12-05 22:13:20 +0800

    add some abstract class code

---
+++ src/main/java/abstractClasses/Person.java
@@ -0,0 +1,17 @@
+package abstractClasses;
+
+/**
+ * @date 12/5/2021 10:00 PM
+ */
+public abstract class Person {
+
+    public abstract  String getDescription();//抽象方法不需要实现，有一个抽象方法，name这个类也必须抽象
+    private String name;
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName(){//抽象类允许不抽象的方法
+        return name;
+    }
+}
